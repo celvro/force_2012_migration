079dacd16cfd9aa3cf80a16814471ca362d9df28
celvro <dwlfk2@mst.edu>
2015-06-17 11:22:16 -0500

    Abandon thread

--- sccm_force_migrate.pl
+++ sccm_force_migrate.pl
@@ -3,30 +3,20 @@
 
 use warnings;
 use strict;
-
-use WWW::Mechanize;
-use HTML::TreeBuilder::XPath;
-use Text::CSV;
 use Getopt::Long;
-use Term::ReadKey;
-use Data::Dumper;
 use Time::HiRes qw(gettimeofday tv_interval);
-use POSIX qw(floor);
-use threads;
-use List::Util;
+
+$SIG{CHLD} = 'IGNORE';
 
 sub usage {
     print qq/
 usage: $0 [--help] [--verbose] [--test]
-         [--threads <n>] [--thread-wait <n>]
-         [--hosts-file <file>] [--hosts=host1,host2,host3,...]
+        [--hosts-file <file>] [--hosts=host1,host2,host3,...]
 /;
 }
 
 my $test_only = 0;
 my $verbose   = 0;
-my $max_threads = 2;
-my $thread_wait = 5;
 
 my @hosts;
 
@@ -36,8 +26,6 @@ GetOptions(
     'verbose!' => \$verbose,
     'test!' => \$test_only,
 
-    'threads=i' => \$max_threads,
-    'thread-wait=i' => \$thread_wait,
     'hosts=s' => sub {
         push(@hosts,split(',',$_[1]));
     },
@@ -71,59 +59,48 @@ if ($test_only)
     exit(0);
 }
 
-print("Using ${max_threads} threads.\n");
 my $start_time = [gettimeofday];
+`echo %DATE% %TIME% >> failed.txt`;
+`echo %DATE% %TIME% >> finished.txt`;
+
 
 sub install_client {
     my $hostname = shift;
-    system( 'psexec -s \\\\'.$hostname.' C:\Perl64\bin\perl.exe '.
-            '\\\\minerfiles.mst.edu\dfs\software\appserv\sccm_2012_client\update-prod-server.pl' );
+    `findstr /c:"$hostname" finished.txt`;
+    if ( $?==0 )
+    {
+        print "$hostname already done.\n";
+        return 1;
+    }
+    
+    
+    `ping -n 1 $hostname`;
+    if ( $?!=0 )
+    {
+        print "  [OFFLINE] $hostname\n";
+        system("echo [OFFLINE] $hostname >> failed.txt");
+        return 0;
+    }
+    
+    my $cmd = system( 'psexec -n 2 -d -s \\\\'.$hostname.' C:\Perl64\bin\perl.exe '.
+                         '\\\\minerfiles.mst.edu\dfs\software\appserv\sccm_2012_client\update-prod-server.pl '.
+                         '2>stderr.txt' );
+    if ( $cmd == 46080 )
+    {
+        print "  [TIMEOUT] $hostname\n";
+        system("echo [TIMEOUT] $hostname >> failed.txt");
+        return 0;
+    }
+    
+    print "$hostname started.\n";
+    system("echo $hostname >> finished.txt");
+    return 1;
 }
 
-# Generate a template for each host in %host_specs.
-my @threads;
+
 foreach my $host (@hosts) {
-    if ($max_threads > 0) {
-        push(@threads,threads->create(\&install_client,
-                                      $host));
-
-        print '*';
-        # Give the thread a head start before launching another one.
-        sleep($thread_wait);
-
-        while (@threads >= $max_threads) {
-            ProcessFinishedThreads(\@threads);
-            
-            print ".";
-            sleep(1);
-        }
-    }
-}
-if ($max_threads > 0) {
-    while (@threads > 0) {
-        ProcessFinishedThreads(\@threads);
-        
-        print ".";
-        sleep(1);
-    }
+    install_client($host);
 }
 
 my $elapsed = tv_interval($start_time, [gettimeofday]);
 print("\nProcess completed in ${elapsed} seconds.\n");
-
-sub ProcessFinishedThreads {
-    my $threads = shift;
-
-    my @tojoin = threads->list(threads::joinable);
-    
-    foreach my $thr (@tojoin) {
-        $thr->join();
-        print("_");
-        for (my $i=0; $i<@$threads; $i++) {
-            if ($threads->[$i]->tid() == $thr->tid()) {
-                splice(@$threads,$i,1);
-                last;
-            }
-        }
-    }
-}
